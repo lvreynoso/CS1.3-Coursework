eaf8435b2491b83d4a2e6428f65d6f234e9451e0
Lucia Reynoso <lvreynoso@protonmail.com>
2019-04-26 18:19:15 -0700

    Fix circular buffer bug with updating read index & add smoke test

--- circularbuffer.py
+++ circularbuffer.py
@@ -2,8 +2,8 @@
 
 class CircularBuffer(object):
     """docstring for CircularBuffer"""
-    def __init__(self, max_size):
-        data = [None] * max_size
+    def __init__(self, max_size=8):
+        self.data = [None] * max_size
         self.max_size = max_size
         self.size = 0
 
@@ -12,28 +12,69 @@ class CircularBuffer(object):
         self.write_index = 0
         self.read_index = 0
 
-    def is_empty():
+    def __str__(self):
+        items = []
+        for index, value in enumerate(self.data):
+            if index == self.write_index and index == self.read_index:
+                items.append('*&{!r}'.format(value))
+            elif index == self.write_index:
+                items.append('&{!r}'.format(value))
+            elif index == self.read_index:
+                items.append('*{!r}'.format(value))
+            else:
+                items.append('{!r}'.format(value))
+        return '{' + ', '.join(items) + '}'
+
+    def is_empty(self):
         return self.size == 0
 
-    def is_full():
+    def is_full(self):
         return self.size == self.max_size
 
-    def enqueue(item):
-        self.data[write_index] = item
-        self.write_index += 1
-        if self.write_index == len(data):
-            self.write_index = 0
+    def enqueue(self, item):
+        if self.data[self.write_index] is not None:
+            self.read_index = (self.read_index + 1) % self.max_size
+        self.data[self.write_index] = item
+        self.write_index = (self.write_index + 1) % self.max_size
         self.size += 1
 
-    def front():
+    def front(self):
         return self.data[self.read_index]
 
-    def dequeue():
+    def dequeue(self):
         front_item = self.front()
         self.data[self.read_index] = None
-        self.read_index += 1
-        if self.read_index == len(data):
-            self.read_index = 0
+        self.read_index = (self.read_index + 1) % self.max_size
         self.size -= 1
         return front_item
 
+def test_circular_buffer():
+    cb = CircularBuffer(4)
+    print('Circular Buffer: ' + str(cb))
+    cb.enqueue('Alpha')
+    print('Circular Buffer: ' + str(cb))
+    cb.enqueue('Beta')
+    print('Circular Buffer: ' + str(cb))
+    cb.enqueue('Gamma')
+    print('Circular Buffer: ' + str(cb))
+    cb.enqueue('Delta')
+    print('Circular Buffer: ' + str(cb))
+
+    cb.enqueue('Epsilon')
+    print('Circular Buffer: ' + str(cb))
+    print(cb.front())
+    cb.dequeue()
+    print('Circular Buffer: ' + str(cb))
+    print(cb.front())
+    cb.dequeue()
+    print('Circular Buffer: ' + str(cb))
+    print(cb.front())
+    cb.dequeue()
+    print('Circular Buffer: ' + str(cb))
+    print(cb.front())
+    cb.dequeue()
+    print('Circular Buffer: ' + str(cb))
+
+
+if __name__ == '__main__':
+    test_circular_buffer()
